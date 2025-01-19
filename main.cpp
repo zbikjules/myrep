079bcb60a0b131d81fb3fe93da8e526737c73ea7
zbikjules <zbikjules@gmail.com>
2025-01-19 20:49:43 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 echo '#include <iostream>\n
 int main() 
-{ std::cout << "Hello, World!"; return 0; }' > main.cpp
+{ std::cout << "Hello, World!"; return 0; }' > main.cpp 
+echo "// dodano komentarz" >> main.cpp
+git commit -am "Zaktualizowano main.cpp, dodano komentarz"
+
 
 
